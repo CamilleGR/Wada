05397d334289f41c42cc35286de4b83df8687525
Oscar Odic <odic.oscar@gmail.com>
2014-12-22 14:35:09 +0100

    Test d'un algo simple Spark sur le web service

--- WebService/src/main/scala/Service.scala
+++ WebService/src/main/scala/Service.scala
@@ -4,6 +4,10 @@ import akka.actor.Actor
 import spray.http.MediaTypes._
 import spray.routing._
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
 class ServiceActor extends Actor with Service {
@@ -30,22 +34,31 @@ trait Service extends HttpService {
     }
     // f
   }
-  val writer = new PrintWriter(new File("test.txt" ))
-  writer.write("Hello Scala") // Exemple concret pour l'écriture d'un fichier avec scala ... Difficulté 5 étoiles
-  writer.close()
+
+  def spark() = {
+    val conf = new SparkConf().setAppName("test").setMaster("local")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile("scripts/data1.csv")
+    textFile.collect()
+  }
+
+  //val writer = new PrintWriter(new File("test.txt" ))
+  //writer.write("Hello Scala") // Exemple concret pour l'écriture d'un fichier avec scala ... Difficulté 5 étoiles
+  //writer.close()
 
 
   val myRoute =
     path("") {
       post { //On définie ce qui est envoyé SI on utilise la méthode POST
         formFields("demande", "nomFichier", "attribut", "segment".as[Int]) { (demande, nomFichier, attribut, segment) => //Les paramètres de la méthode POST sont mentionnés par la fonction formFields
+          var test = spark()
           respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
             complete {
-              // on ajoute un bloc {} pour mettre notre variable : Voir ligne 34
+              // on ajoute un bloc {} pour mettre notre variable
               <html>
                 <body>
                   <p>Utilisation de POST :</p>
-
+                  <p>{test}</p>
                   <p>{demande} {nomFichier} {attribut} {segment}</p>
                 </body>
               </html>
