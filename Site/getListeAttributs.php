8fbd12971c859f127add82d8e5a12281daa2dabb
Camille Gerin-Roze <gerinrozecamille@yahoo.fr>
2014-12-26 19:17:05 +0100

    Ajout d'un script php qui écrit un input de type hidden qui renvoie une liste d'attributs dans ['listeAttributs']

--- Site/getListeAttributs.php
+++ Site/getListeAttributs.php
@@ -2,5 +2,5 @@
 $chaine = "Ali est vraiment un gros PD; En plus il suce des bites; il ne peux pas s\'en empêcher; c'est un putain de sodomite";
 // à noter la beauté des rimes.
 
-echo '<form method="GET"><input type="hidden" name="listeAttributs" value="'.$chaine.'"/> </form>';
+echo '<html> <body><form method="GET"><input type="hidden" name="listeAttributs" value="'.$chaine.'"/><input type="submit"/></form></body></html>';
 ?>
