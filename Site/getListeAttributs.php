47eed9522649e99a842ca0ca621a36cf98d1b9c4
ali.karami <ali_92000@hotmail.fr>
2014-12-27 11:55:15 +0100

    MAJ du site

--- Site/getListeAttributs.php
+++
@@ -1,6 +0,0 @@
-<?php
-$chaine = "Ali est vraiment un gros PD; En plus il suce des bites; il ne peux pas s\'en empêcher; c'est un putain de sodomite";
-// à noter la beauté des rimes.
-
-echo '<html> <body><form method="GET"><input type="hidden" name="listeAttributs" value="'.$chaine.'"/><input type="submit"/></form></body></html>';
-?>
