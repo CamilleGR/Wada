b21aa62f89f2efa51ea241ef98f0b605f2760ce2
Camille Gerin-Roze <gerinrozecamille@yahoo.fr>
2015-01-03 21:32:19 +0100

    Mise à jour de la fonction isNumeric qui marche maintenant pour un RDD[Array[String]]

--- AlgoScala/WadaProject/src/numeric.scala
+++ AlgoScala/WadaProject/src/numeric.scala
@@ -4,6 +4,9 @@ def isNumeric(str: String): Boolean = {
  
 //"(-?[0-9]+(?:\\.[0-9]+)?)"
 
+/*
+* Fonction sur un Tableau 2D
+*
 
 def isNumericAttr(col:Int,array:Array[Array[String]]):Boolean = {
 	var test = true;
@@ -11,4 +14,15 @@ def isNumericAttr(col:Int,array:Array[Array[String]]):Boolean = {
 			test &= isNumeric(array(i)(col))
 		}
 	return test;
+}
+*/
+
+
+
+/*
+*	Fonction qui marche sur un RDD déjà splité 
+*	Prend en paramètre la colonne sur laquelle il faut faire le test
+*/
+def isNumericAttr(array:org.apache.spark.rdd.RDD[Array[String]], col:Int):Boolean ={
+	return array.map(r => r(col)).filter(line => !isNumeric(""+line)).count() <= 1	
 }
