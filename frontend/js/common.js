3d3360aecfae1a59d6ee7c2e28d99dfb8c33204c
ITZ hugo <hugo.sanson@edu.univ-paris13.fr>
2015-02-03 00:50:57 +0100

    updateToday

---
+++ frontend/js/common.js
@@ -0,0 +1,32 @@
+/**
+ * @author user
+ */
+
+function openFiles() {
+	$('#open2').popover('hide');
+}
+
+function progressUp(barId, val) {
+	$(barId).css("width", "" + val + "%");
+}
+
+
+/*Snippet For random background color*/
+
+function get_random_color() {
+	var letters = 'ABCDE'.split('');
+	var color = '#';
+	for (var i = 0; i < 3; i++) {
+		color += letters[Math.floor(Math.random() * letters.length)];
+	}
+	return color;
+}
+
+function get_random_color_set(nb) {
+	var colors = [];
+	var i = 0;
+	for (i; i < nb; i++) {
+		colors.push(get_random_color());
+	}
+	return colors;
+}
