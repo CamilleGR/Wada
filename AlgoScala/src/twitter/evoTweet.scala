1aecf6246667986a2913ee2405498491845aed81
Camille Gerin-Roze <gerinrozecamille@yahoo.fr>
2015-02-11 22:47:38 +0100

    A refaire car pas assez opti ... je n'arrives même pas à les faires tourner parce que mon ordi à la flemme ! FUCK IT

--- AlgoScala/src/twitter/evoTweet.scala
+++ AlgoScala/src/twitter/evoTweet.scala
@@ -12,7 +12,7 @@ def evolutionParMinute(sc:SparkContext, fileName:String): RDD[(String,Int)] = re
 def evolutionParTranche(sc:SparkContext, tranche:Int,  array:RDD[(String,Int)]): Array[(String,Int)] = {
 
 
-	c = Calendar.getInstance();
+	var c = Calendar.getInstance();
 	var tab = ArrayBuffer.empty[(String,Int)]
 	var min = array.map(x => x._1).min
 	var max = array.map(x => x._1).max
@@ -30,18 +30,18 @@ def evolutionParTranche(sc:SparkContext, tranche:Int,  array:RDD[(String,Int)]):
 
 def evolutionParTranche2(sc:SparkContext, tranche:Int, fileName:String): Array[(String,Int)] = {
 
-	c = Calendar.getInstance();
+	var c = Calendar.getInstance();
 	var tab = ArrayBuffer.empty[(String,Int)]
-	var min = array.map(x => x._1).min
-	var max = array.map(x => x._1).max
 	var file = sc.textFile(fileName).map(x => x.split(";")(0)).filter(x=> x matches """[0-9]{2}:[0-9]{2}""")
+	var min = file.min
+	var max = file.max
 	c.set(Calendar.HOUR_OF_DAY,min.split(":")(0).toInt)
 	c.set(Calendar.MINUTE,min.split(":")(1).toInt)
 		while(c.get(HOUR_OF_DAY)+":"+c.get(MINUTE)<=max){
 			var inf = c.get(HOUR_OF_DAY)+":"+c.get(MINUTE)
 			c.add(MINUTE,tranche)
 			var sup = c.get(HOUR_OF_DAY)+":"+c.get(MINUTE)
-			tab += (inf,file.filter(x=> x._1 >= inf && x._1<=sup).count)
+			tab append file.filter(line => inf <= line &&line<=sup).map(x => (inf+"-"+sup,1)).reduceByKey((x,y)=>x+y).first
 		}
-		return tab
+		return tab.toArray
 }
