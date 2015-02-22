d1a8f1808d57a31263d53e54c0ce4849a95d5d2d
Camille Gerin-Roze <gerinrozecamille@yahoo.fr>
2015-02-22 16:39:44 +0100

    ajout de la fonction TimeFormat me permettant de mettre le temps dans un format plus facilement traitable

--- WebService/src/main/scala/fonctions/TwitterStream.scala
+++ WebService/src/main/scala/fonctions/TwitterStream.scala
@@ -55,18 +55,53 @@ class TwitterStream (sparkContext:SparkContext){
   *	de ces cles.
   *	Le flux se deroule pour un certain temps, apres il faudra faire une fonction qui permet d'executer le flux pour un temps indefini, puis
   *	une fonction pour arreter ce flux.
-
+  */
   def creerStream(tag:Array[String],path:String,time:Int):Unit ={
 
     var ssc = new StreamingContext(sc,Seconds(60))
     var jssc= new JavaStreamingContext(ssc)
     var tweets = TwitterUtils.createStream(jssc,auth,tag,StorageLevel.MEMORY_ONLY) //Creation du flux
 
-    tweets.dstream.map(x=> x.getCreatedAt.getDay+"/"+x.getCreatedAt.getMonth+"/"+x.getCreatedAt.getYear+" "+x.getCreatedAt.getHours+":"+x.getCreatedAt.getMinutes + " ; " + x.getText.split(" ").filter(_.startsWith("#")).mkString(" ").replace(";","")).saveAsTextFiles(path)
+    tweets.dstream.map(x=> timeFormat(x.getCreatedAt.toString) +" ; " + x.getText.split(" ").filter(_.startsWith("#")).mkString(" ").replace(";","")).saveAsTextFiles(path)
 
     jssc.start
     Thread.sleep(time*60000)	// on attend time minutes pour couper le flux
     jssc.stop(stopSparkContext=false)
-  }*/
+  }
+  
+  def timeFormat(str:String):String ={
+		var tab = str.split(" ");
+		tab(0)=tab(5)
+		tab(4)=""
+		tab(5)=""
+		tab(3)=tab(3).substring(0,5)
+		if(tab(1).equals("Jan")){
+			tab(1)="01"
+		}else if(tab(1).equals("Feb")){
+			tab(1)="02"
+		}else if(tab(1).equals("Mar")){
+			tab(1)="03"
+		}else if(tab(1).equals("Apr")){
+			tab(1)="04"		
+		}else if(tab(1).equals("May")){
+			tab(1)="05"		
+		}else if(tab(1).equals("Jun")){
+			tab(1)="06"		
+		}else if(tab(1).equals("Jul")){
+			tab(1)="07"		
+		}else if(tab(1).equals("Aug")){
+			tab(1)="08"		
+		}else if(tab(1).equals("Sep")){
+			tab(1)="09"		
+		}else if(tab(1).equals("Oct")){
+			tab(1)="10"		
+		}else if(tab(1).equals("Nov")){
+			tab(1)="11"		
+		}else if(tab(1).equals("Dec")){
+			tab(1)="12"		
+		}
+		return tab.mkString("")
+	}
+	
 
 }
