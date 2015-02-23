bda465fb6f67856f6f3a7c77d9e2c9f6229edf01
ali.karami <ali_92000@hotmail.fr>
2015-02-23 14:06:52 +0100

    maj

---
+++ SiteFinal/accueil.php
@@ -0,0 +1,26 @@
+<!doctype html>  
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Ali - Accueil</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+  </head>
+
+
+  <body> 
+    <?php include "menu.html"; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
