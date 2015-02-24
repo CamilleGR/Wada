cabd5e6de7b41a76579ad520006f091f77706d81
ali.karami <ali_92000@hotmail.fr>
2015-02-24 10:41:25 +0100

    ajout support courbe et diagrame

---
+++ SiteFinal/import.php
@@ -0,0 +1,48 @@
+<!doctype html>  
+<html>
+	<head>
+	    <meta charset="UTF-8">
+	    <title>Wada - importation</title>
+    	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	</head>
+
+	<style>
+	/* STYLE pour le bouton importation */
+	.file {
+	  position: relative;
+	}
+	.file label { /* affichage du css */
+	  background: #333333;
+	  padding: 5px 20px;
+	  color: #6ACD79;
+	  font-weight: bold;
+	  font-size: .9em;
+	  transition: all .4s;
+	}
+	.file input { /* cache le bouton avec le css */
+	  position: absolute;
+	  display: inline-block;
+	  left: 0;
+	  top: 0;
+	  opacity: 0.01;
+	  cursor: pointer;
+	}
+	.file input:hover + label,
+	.file input:focus + label {
+	  background: #6ACD79;
+	  color: #333333;
+	}
+	</style>
+
+	<body> 
+	    <?php include "menu.html"; ?>
+
+		<form action="#">
+		  <p class="file">
+		    <input type="file" name="file" id="file" />
+		    <label for="file">Importer</label>
+		  </p>
+		</form>
+		
+	</body>
+</html>
---
+++ SiteFinal/js/upload.js
@@ -0,0 +1,167 @@
+// make dataset globally available
+var dz;
+var chart;
+
+function setChart(index){
+	chart = index;
+}
+function getChart(){
+	return chart;
+}
+
+// load dataset and create table
+function load_dataset(csv) {
+  var data = d3.csv.parse(csv);
+  dz=data;
+  create_table(data);
+  progressUp(".progress-bar", 50);
+  loadHisto(data);
+  //console.log("1");
+  setChart(1);		
+  
+  d3.select("#visuelChng").remove();
+  notif();
+  
+  var uli = $('<li></li>').attr("role","presentation");
+  	var test = $('<a></a>').attr("id","visuelChng").click(function(){
+  	//alert(chart);
+  	setChart(changeGraph(getChart(),dz));
+  	notif();
+  	uli.addClass("active");
+  		
+  });
+  test.html('<p><span class="glyphicon glyphicon-refresh"></span>'+' Changer de visuel</p>');
+  uli.append(test);
+  $("#tools").append(uli);
+  
+  console.log("%"+chart);
+  //console.log(test.parent().html());
+   
+  progressUp(".progress-bar", 100);
+}
+
+function notif(){
+	$("#tll1").html("");
+  	var min = $("<span></span>").attr("class","glyphicon glyphicon-minus");
+  	var selecTtl = $("#tll1");
+  	if(getChart()==1){
+	  	selecTtl.text(" Histogramme ");
+	  }else{
+	  	selecTtl.text(" Diagramme ");
+	  }
+	  selecTtl.prepend(min);
+}
+
+function changeGraph(chart,data){
+
+	
+  	if(chart==1){
+  		loadPie(dz);
+  		return 2;
+  	}else if(chart==2){
+  		loadHisto(dz);
+  		return 1;
+  	}else{
+  		console.log("Chart = 1 loadPie\nChart = 2 loadHisto");
+  	}
+  	
+  }
+
+/*
+  $("#collapseOne").removeClass("in");
+  $("#collapseOne").addClass("collapsing");
+  $("#collapseOne").removeClass("collapsing");
+  $("#collapseOne").addClass("collapse");
+  */
+
+function range(start, end) {
+    var foo = [];
+    for (var i = start; i <= end; i++) {
+        foo.push(i);
+    }
+    return foo;
+}
+
+function addItemDD(list,name){
+	var item = list.append("li").append("a").attr("role","menuitem");
+	item.text(name);
+	return item;
+}
+//itemArray.forEach(function(d){});
+
+
+function createDropdown(container,name){
+	
+	var stats = d3.select(container);
+    
+	var group = stats.append("div").attr("class","dropdown btn-group").attr("role","group");
+    
+    var btn = group.append("button").attr("id","dLabel").attr("class","btn btn-default btn-xs").attr("aria-haspopup","button").attr("data-toggle","dropdown").attr("aria-haspopup","true").attr("aria-expanded","false");
+    
+    btn.text(name);
+    btn.append("span").attr("class","caret");
+    var listAttr = group.append("ul").attr("class","dropdown-menu").attr("aria-labelledby","dLabel").attr("role","menu");
+	
+    
+    $('.dropdown-toggle').dropdown();
+    return listAttr;
+}
+
+// create a table with column headers, types, and data
+function create_table(data) {
+  // table stats
+  var keys = d3.keys(data[0]);
+  
+  
+  var name = "A propos de "+keys[0];
+  
+  var info = d3.select("#statsTitle").text(name);
+  var stats = d3.select("#stats").html("");
+    	
+    stats.append("div")
+    .text("Columns : " + keys.length)
+
+  stats.append("div")
+    .text("Rows : " + data.length)
+    stats.append("hr");
+    
+
+
+  d3.select("#table")
+    .html("")
+    .append("tr")	
+    .selectAll("th")
+    .data(keys)
+    .enter().append("th").attr("class","thead")
+      .text(function(d) { return d; });
+
+  d3.select("#table")
+    .selectAll("tr")
+      .data(data)
+    .enter().append("tr")
+    
+      .attr("class", "linetable")
+      .selectAll("td")
+      .data(function(d) { return keys.map(function(key) { return d[key];	 }) ; })
+      .enter().append("td").attr("class","tcell")       .text(function(d) { return d; });
+      
+}
+
+// handle upload button
+function upload_button(el, callback) {
+  var uploader = document.getElementById(el);  
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    callback(contents);
+  };
+
+  uploader.addEventListener("change", handleFiles, false);  
+
+  function handleFiles() {
+    d3.select("#table").text("loading...");
+    var file = this.files[0];
+    reader.readAsText(file);
+  };
+};
