d30eb455bde87219e72fe19c8910918ea051eccc
Camille Gerin-Roze <gerinrozecamille@yahoo.fr>
2015-02-24 23:24:38 +0100

    commit

--- WebService/src/main/scala/fonctions/TwitterStream.scala
+++ WebService/src/main/scala/fonctions/TwitterStream.scala
@@ -63,7 +63,38 @@ class TwitterStream(sparkContext: SparkContext) extends java.io.Serializable {
     val tsu = new TwitterStreamUtil
 
 
-    tweets.dstream.map(x => tsu.timeFormat(x.getCreatedAt.toString) + " ; " + x.getText.split(" ").filter(_.startsWith("#")).mkString(" ").replace(";", "")).saveAsTextFiles(path)
+    tweets.dstream.map(x => {
+      var tab = x.getCreatedAt.toString.split(" ");
+      tab(0) = tab(5)
+      tab(4) = ""
+      tab(5) = ""
+      tab(3) = tab(3).substring(0, 5)
+      if (tab(1).equals("Jan")) {
+        tab(1) = "01"
+      } else if (tab(1).equals("Feb")) {
+        tab(1) = "02"
+      } else if (tab(1).equals("Mar")) {
+        tab(1) = "03"
+      } else if (tab(1).equals("Apr")) {
+        tab(1) = "04"
+      } else if (tab(1).equals("May")) {
+        tab(1) = "05"
+      } else if (tab(1).equals("Jun")) {
+        tab(1) = "06"
+      } else if (tab(1).equals("Jul")) {
+        tab(1) = "07"
+      } else if (tab(1).equals("Aug")) {
+        tab(1) = "08"
+      } else if (tab(1).equals("Sep")) {
+        tab(1) = "09"
+      } else if (tab(1).equals("Oct")) {
+        tab(1) = "10"
+      } else if (tab(1).equals("Nov")) {
+        tab(1) = "11"
+      } else if (tab(1).equals("Dec")) {
+        tab(1) = "12"
+      }
+      tab.mkString("")} + " ; " + x.getText.split(" ").filter(_.startsWith("#")).mkString(" ").replace(";", "")).saveAsTextFiles(path)
 
     jssc.start
     Thread.sleep(time * 60000) // on attend time minutes pour couper le flux
