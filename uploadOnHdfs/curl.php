213af35dc4af22ded8dab24ed74857b5e471f7e4
RinDman <rindman@gmail.com>
2015-03-13 11:00:11 +0100

    JSON header

--- uploadOnHdfs/curl.php
+++ uploadOnHdfs/curl.php
@@ -2,6 +2,7 @@
 	header("Access-Control-Allow-Origin:http://localhost:50070"); 
 	header("Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, PATCH, DELETE");
 	header("Access-Control-Allow-Headers : Content-Type,api_key,Authorization,X-Auth-Token");
+	header('Content-type: application/json');
 	if($_GET == null)
 	{
 		echo '<html><body>Erreur 404</body></html>';
