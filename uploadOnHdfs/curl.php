f88320c5b648e8a9db563ee7ddd5dbe495d259f1
RinDman <rindman@gmail.com>
2015-03-13 10:53:27 +0100

    Verification des paramètres reçus

--- uploadOnHdfs/curl.php
+++ uploadOnHdfs/curl.php
@@ -1,16 +1,17 @@
 <?php
 	header("Access-Control-Allow-Origin:http://localhost:50070"); 
 	header("Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, PATCH, DELETE");
-    header("Access-Control-Allow-Headers : Content-Type,api_key,Authorization,X-Auth-Token");
+	header("Access-Control-Allow-Headers : Content-Type,api_key,Authorization,X-Auth-Token");
 	if($_GET == null)
 	{
 		echo '<html><body>Erreur 404</body></html>';
 	}
 	
-	else
+	else if(isset($_GET['adr']))
 	{
 		$result = file_get_contents( 'http://'.$_GET['adr']);
-		
-		echo $result;
-		
+		if($result)
+		{
+			echo $json_encode(result);
+		}
 	}
