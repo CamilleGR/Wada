0a5865734d1f4d2285f9e532e56b6b19b384719b
RinDman <rindman@gmail.com>
2015-03-13 11:02:57 +0100

    Requete mis à jour
    Return boolean object if the get fails

--- uploadOnHdfs/curl.php
+++ uploadOnHdfs/curl.php
@@ -3,16 +3,17 @@
 	header("Access-Control-Allow-Methods : POST, GET, OPTIONS, PUT, PATCH, DELETE");
 	header("Access-Control-Allow-Headers : Content-Type,api_key,Authorization,X-Auth-Token");
 	header('Content-type: application/json');
-	if($_GET == null)
-	{
-		echo '<html><body>Erreur 404</body></html>';
-	}
-	
-	else if(isset($_GET['adr']))
+
+	if(isset($_GET['adr']))
 	{
 		$result = file_get_contents( 'http://'.$_GET['adr']);
 		if($result)
 		{
-			echo $json_encode(result);
+			echo json_encode(result);
 		}
+	}
+	else
+	{
+		$result = array[ 'ok' => false]
+		echo json_encode($result);
 	}
