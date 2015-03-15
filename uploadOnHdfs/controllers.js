256b8413fe52654b29778ea7ce3350769205738b
ITZ hugo <hugo.sanson@edu.univ-paris13.fr>
2015-03-15 16:30:29 +0100

    Angular list File statuses

---
+++ uploadOnHdfs/controllers.js
@@ -0,0 +1,36 @@
+var listFiles = angular.module('listFiles', []);
+
+listFiles.controller('listFilesCtrl', function ($scope) {
+  
+  
+$scope.fileStatuses =
+    [
+      {
+        "accessTime"      : 1320171722771,
+        "blockSize"       : 33554432,
+        "group"           : "supergroup",
+        "length"          : 24930,
+        "modificationTime": 1320171722771,
+        "owner"           : "webuser",
+        "pathSuffix"      : "a.patch",
+        "permission"      : "644",
+        "replication"     : 1,
+        "type"            : "FILE"
+      },
+      {
+        "accessTime"      : 0,
+        "blockSize"       : 0,
+        "group"           : "supergroup",
+        "length"          : 0,
+        "modificationTime": 1320895981256,
+        "owner"           : "szetszwo",
+        "pathSuffix"      : "bar",
+        "permission"      : "711",
+        "replication"     : 0,
+        "type"            : "DIRECTORY"
+      }
+    ];
+  
+
+});
+
