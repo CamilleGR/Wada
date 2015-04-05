bd72c324bac15254ed42087e7f4d0f2de01166f0
ali.karami <ali_92000@hotmail.fr>
2015-04-05 15:10:41 +0200

    maj traitementStream.php

--- SiteFinal/traitementStream.php
+++ SiteFinal/traitementStream.php
@@ -3,23 +3,38 @@
 	<head>
     <meta charset="UTF-8">
     <title>Wada - API Twitter</title>
+
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
     <link rel="stylesheet" type="text/css" href="css/bootstrap-formhelpers.min.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic"  id="style-resource-3">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,600,200italic,600italic&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/styletweet.css"  id="style-resource-4">
+
     <!-- Style particulier pour les titre h2  -->
 	<style type="text/css">
 		@font-face {  font-family: 'quadranta';		src: url('fonts/aspace_demo.otf') format('truetype');	font-weight: normal;	font-style: normal; }	 
 		h2 {  font-family:quadranta, sans-serif;	font-size:3em; }
 		h3{	font-family:quadranta, sans-serif;	font-size:2em; }
+
+		.bubbleChart {
+	      min-width: 100px;
+	      max-width: 500px;
+	      height: 500px;
+	      margin: 0 auto;
+	    }
+	    .bubbleChart svg{
+	      background: #fff;
+	    }
 	</style>
   </head>
 
 
   <body>
+
     <?php include "menu.html"; ?>
 
 	
-
 		<!-- Header de la page  -->
 		<div class="jumbotron">
 			<div class="container">
@@ -34,7 +49,7 @@
 			</center>
 		</div>
 		
-	<p> Entrez le chemin correspondant à votre enregistrement et l'interval de temps que vous souhaitez pour visualiser l'evolution de votre hashtag </p>
+	<center><p> Entrez le chemin correspondant à votre enregistrement et l'interval de temps que vous souhaitez pour visualiser l'evolution de votre hashtag </p></center>
 	
 	<div id="formulaire" style="display:none;">
 		<form  method="GET" action="api2.php" style="z-index:98; position:relative; top:auto;  width:300px; height: 18px; margin-left: 39%;" >
@@ -49,8 +64,71 @@
 			</div>
 		</form>
 	</div>
-	<script src="js/jquery-1.10.2.min.js"></script>
+
+
+
+
+
+	<!-- evolution du tweet -->
+	<div id="courbes" style="display:none;">
+		<div class="page-container" >	
+			<div class="main-content">
+				<div class="row">
+					<div class="col-md-12">		
+						<div class="panel panel-primary">
+							
+							<table class="table table-bordered">
+								<tbody>
+				                    <tr>
+										<td>
+											<div id="morrisline" style="height: 300px"></div>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+							
+						</div>	
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div id="bulles" style="display:none;">
+		<div class="center-block" >
+			<p><button class="btn btn-success btn-large" onclick="association()">Hashtags associés</button></p>
+			<div class="bubbleChart"></div>
+		</div>
+	</div>
+
+
+
+  	<!-- pour le traitement des graphes -->
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="js/charts.js"></script> <!-- fait les requetes pour levolution des tweets -->
+	<script src="js/raphael-min.js"></script>
+	<script src="js/morris.min.js"></script>
+  	<script src="js/indexBulle.js"></script> <!--  fait les requetes pour les hashtags  -->
+
+	<!-- pour les bibliotheques -->
+	<script src="http://phuonghuynh.github.io/js/bower_components/jquery/dist/jquery.min.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/d3/d3.min.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/d3-transform/src/d3-transform.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/cafej/src/extarray.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/cafej/src/misc.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/cafej/src/micro-observer.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/microplugin/src/microplugin.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/bubble-chart/src/bubble-chart.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/bubble-chart/src/plugins/central-click/central-click.js"></script>
+  	<script src="http://phuonghuynh.github.io/js/bower_components/bubble-chart/src/plugins/lines/lines.js"></script>
+
+  	<!-- pour le traitement du stream -->
 	<script src="js/traitementStream.js">/*C'est dans le fichier qu'on modifiera le script*/</script>
+
+
+
 	
 	</body>
 </html>
