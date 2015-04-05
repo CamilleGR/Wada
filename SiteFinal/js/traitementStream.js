36453a9e2a292be6f1ef14c8d8fc24ec17fa8e5a
Camille Gerin-Roze <gerinrozecamille@yahoo.fr>
2015-04-05 13:50:22 +0200

    Javascript avec fondu savoyarde

--- SiteFinal/js/traitementStream.js
+++ SiteFinal/js/traitementStream.js
@@ -1,10 +1,11 @@
 $(document).ready( function(){
 
-
-
-
-
-
+	$('#formulaire').fadeIn();
+	
+	$('#boutonSubmit').click(function(e){
+//href="proxyWebService.php" 
+		$('#formulaire').fadeOut();
+	});
 
 
 
